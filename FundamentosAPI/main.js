ffe33ebaebedf9cd33aabface56a27d4b44f16f1
alfredobrade <alfredobrade@gmail.com>
2024-02-21 22:30:20 +0000

    wip

--- FundamentosAPI/main.js
+++ FundamentosAPI/main.js
@@ -3,7 +3,7 @@ console.log("hello")
 const API_key = 'live_XRF86ZUtutf6TJhQXSv0zmS9TcRQkUroMjssXChOVxqZRFPHXCW0QAi6NsnVUuqt';
 const API_key_query_param= 'api_key=live_XRF86ZUtutf6TJhQXSv0zmS9TcRQkUroMjssXChOVxqZRFPHXCW0QAi6NsnVUuqt';
 
-const API_URL = 'https://api.thecatapi.com/v1/images/search';
+const API_URL = 'https://api.thecatapi.com/v1';
 
 
 // fetch(API_URL)
@@ -15,8 +15,8 @@ const API_URL = 'https://api.thecatapi.com/v1/images/search';
 
 //     });
 
-async function reload() {
-    const res = await fetch(`${API_URL}?limit=3&${API_key_query_param}`)
+async function loadRandomMichis() {
+    const res = await fetch(`${API_URL}/images/search?limit=2`)
     const data = await res.json()
     
     const img1 = document.getElementById('img1');
@@ -24,8 +24,19 @@ async function reload() {
     
     const img2 = document.getElementById('img2');
     img2.src = data[1].url;
-    
-    const img3 = document.getElementById('img3');
-    img3.src = data[2].url;
 };
 
+async function loadFavouritesMichis() {
+    const res = await fetch(`${API_URL}/favourites?limit=2&${API_key_query_param}`)
+    const data = await res.json()
+    
+    const img1 = document.getElementById('img1');
+    img1.src = data[0].url;
+    
+    const img2 = document.getElementById('img2');
+    img2.src = data[1].url;
+};
+
+
+loadRandomMichis();
+loadFavouritesMichis();
