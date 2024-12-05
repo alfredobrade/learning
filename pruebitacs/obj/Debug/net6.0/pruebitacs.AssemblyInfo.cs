5cca61b2e8d53ce909afa249bf1729f09d4b6105
alfredobrade <alfredobrade@gmail.com>
2024-12-05 15:14:49 +0000

    wip

--- pruebitacs/obj/Debug/net6.0/pruebitacs.AssemblyInfo.cs
+++ pruebitacs/obj/Debug/net6.0/pruebitacs.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("pruebitacs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afa519a7512a11dcd2a1803183f94f01ba63332f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+651f38a5d90cd4df361f2746aebb96b603fc235b")]
 [assembly: System.Reflection.AssemblyProductAttribute("pruebitacs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("pruebitacs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
